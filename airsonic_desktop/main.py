d99c78af4cd3b7ea2fb9493ae92e8f27259d5e02
ripdog <ripdog@users.noreply.github.com>
2019-11-23 04:23:29 +1300

    UI expanded. Can now retrieve a list of random albums, then display the properties and tracks of a selected album.

--- airsonic_desktop/main.py
+++ airsonic_desktop/main.py
@@ -1,20 +1,27 @@
-from PySide2.QtCore import QThread, Slot, QModelIndex, Signal
-from PySide2.QtGui import QStandardItemModel, QStandardItem
-from PySide2.QtWidgets import QApplication, QMainWindow
+from datetime import timedelta
+
+from PyQt5.QtCore import QThread, pyqtSlot, QModelIndex, pyqtSignal, QObject, QSize
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 import config
 from networkWorker import networkWorker
 from ui_mainwindow import Ui_AirsonicDesktop
 
 
+class MainWindowSignals(QObject):
+	loadAlbumsOfType = pyqtSignal(str)
+	loadAlbumWithId = pyqtSignal(int)
+
+
 class MainWindow(QMainWindow):
-	loadRandomAlbums = Signal()
 
 	def __init__(self, *args, **kwargs):
 		super(MainWindow, self).__init__(*args, **kwargs)
 		self.networkThread = QThread()
 		self.networkWorker = networkWorker()
 		self.networkWorker.moveToThread(self.networkThread)
+		self.networkThread.start()
 		self.ui = Ui_AirsonicDesktop()
 		self.ui.setupUi(self)
 
@@ -22,48 +29,118 @@ class MainWindow(QMainWindow):
 
 		self.ui.stackedWidget.setCurrentIndex(0)
 
+		self.signals = MainWindowSignals()
 		self.ui.connectButton.clicked.connect(lambda: self.networkWorker.connectToServer(
 			self.ui.domainInput.text(),
 			self.ui.usernameInput.text(),
 			self.ui.passwordInput.text()
 		))
+		self.networkWorker.returnAlbums.connect(self.displayLoadedAlbums)
 		self.networkWorker.connectResult.connect(self.connectResult)
-		self.loadRandomAlbums.connect(self.networkWorker.loadRandomAlbums)
+		self.signals.loadAlbumsOfType.connect(self.networkWorker.getAlbumsOfType)
+		self.signals.loadAlbumWithId.connect(self.networkWorker.getAlbumSongs)
+		self.networkWorker.returnAlbumSongs.connect(self.displayLoadedSongs)
+
 
 	def populateConnectFields(self):
 		self.ui.domainInput.setText(config.config['domain'])
 		self.ui.usernameInput.setText(config.config['username'])
 		self.ui.passwordInput.setText(config.config['password'])
 
-	@Slot()
+	@pyqtSlot(bool)
 	def connectResult(self, success):
+		print('got connect result: ')
+		print(success)
 		if success:
 			self.ui.stackedWidget.setCurrentIndex(1)
-			self.populateAlbumList()
+			self.populatePlayerUI()
 		else:
 			pass
-
 	# should slap an error somewhere lol
 
-	def populateAlbumList(self):
-		items = []
-		self.albumListModel = QStandardItemModel()
+	def populatePlayerUI(self):
+		# Populate icons
+		self.ui.nextTrack.setIcon(QIcon('icons/baseline-skip-next.svg'))
+		self.ui.prevTrack.setIcon(QIcon('icons/baseline-skip-previous.svg'))
+		self.ui.playPause.setIcon(QIcon('icons/baseline-play-arrow.svg'))
+		self.ui.stop.setIcon(QIcon('icons/baseline-stop.svg'))
+		self.ui.search.setClearButtonEnabled(True)
+		countIconPixmap = QIcon('icons/audio-spectrum.svg').pixmap(QSize(24, 24))
+		lengthIconPixmap = QIcon('icons/baseline-access-time.svg').pixmap(QSize(24, 24))
+		yearIconPixmap = QIcon('icons/sharp-date-range.svg').pixmap(QSize(24, 24))
+		self.ui.selectedAlbumReleaseYearIcon.setPixmap(yearIconPixmap)
+		self.ui.selectedAlbumTrackCountIcon.setPixmap(countIconPixmap)
+		self.ui.selectedAlbumTotalLengthIcon.setPixmap(lengthIconPixmap)
+		# Populate Left Panel
+		self.albumTreeListModel = QStandardItemModel()
 		for item in ['Playlists', 'Random', 'Recently Added', 'Artists', 'Albums', 'Folders']:
 			standardItem = QStandardItem(item)
 			if not item == "Random":
 				standardItem.appendRow(QStandardItem('Loading...'))
-			self.albumListModel.appendRow(standardItem)
-		self.ui.albumTreeList.setModel(self.albumListModel)
-		self.ui.albumTreeList.setHeaderHidden(True)
+			self.albumTreeListModel.appendRow(standardItem)
+		self.ui.albumTreeList.setModel(self.albumTreeListModel)
+		self.ui.albumTreeList.setHeaderHidden(False)
 		self.ui.albumTreeList.clicked[QModelIndex].connect(self.albumListClick)
+		self.albumTreeListModel.setColumnCount(2)
+		self.albumTreeListModel.setHorizontalHeaderLabels(["Album", "Artists"])
+		self.ui.albumTreeList.setUniformRowHeights(True)
+		self.ui.albumTreeList.setColumnWidth(0, 300)
+		self.ui.albumTreeList.setIndentation(0)
+		# populate right panel
+		self.albumTrackListModel = QStandardItemModel()
+		self.ui.albumTrackList.setModel(self.albumTrackListModel)
+		self.albumTrackListModel.setHorizontalHeaderLabels(['Track No.', 'Title', 'Artist'])
+		self.ui.albumTrackList.setItemsExpandable(False)
+		self.ui.albumTrackList.setIndentation(0)
 
 	def albumListClick(self, index):
-		item = self.albumListModel.itemFromIndex(index)
+		item = self.albumTreeListModel.itemFromIndex(index)
 		text = item.text()
+		print('{} clicked, attempting load...'.format(text))
 		if text == "Loading...":
 			pass
 		elif text == "Random":
+			self.signals.loadAlbumsOfType.emit("random")
+		else:
+			if item.data(1):
+				data = int(item.data(1))
+				print('got data {}'.format(data))
+				self.signals.loadAlbumWithId.emit(data)
 
+	@pyqtSlot(object, str)
+	def displayLoadedAlbums(self, albums, type):
+		print(albums)
+		destination = self.albumTreeListModel.findItems(type.capitalize())
+		for item in albums['albumList2']['album']:
+			print(item)
+			standarditem = [QStandardItem(item['name']), QStandardItem(item['artist'])]
+			standarditem[0].setData(item['id'],
+									1)  # 1 is the 'data role'. I'm not sure what it is, perhaps a way to store
+			# multiple types of data in a single item?
+			standarditem[1].setData(item['id'], 1)
+			destination[0].appendRow(standarditem)
+
+	@pyqtSlot(object)
+	def displayLoadedSongs(self, album):
+		print(album)
+		self.albumTrackListModel.clear()
+		albumdeets = album['album']
+		albumsongs = albumdeets['song']
+		self.ui.selectedAlbumTitle.setText(albumdeets['name'])
+		self.ui.selectedAlbumArtist.setText(albumdeets['artist'])
+		self.ui.selectedAlbumTrackCount.setText(str(albumdeets['songCount']))
+		self.ui.selectedAlbumTotalLength.setText(str(timedelta(seconds=albumdeets['duration'])))
+		try:
+			self.ui.selectedAlbumReleaseYear.setText(str(albumdeets['year']))
+		except KeyError:
+			self.ui.selectedAlbumReleaseYear.setText('')
+		for song in albumsongs:
+			items = [QStandardItem(str(song['track'])),
+					 QStandardItem(song['title']),
+					 QStandardItem(song['artist'])]
+			for item in items:
+				item.setData(song['id'], 1)
+			self.albumTrackListModel.appendRow(items)
 
 if __name__ == "__main__":
 	app = QApplication([])
--- airsonic_desktop/networkWorker.py
+++ airsonic_desktop/networkWorker.py
@@ -1,24 +1,34 @@
-from PySide2.QtCore import QObject, Slot, Signal
+from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
 from libsonic import Connection
 
 
 class networkWorker(QObject):
 	# whether the server could be connected to, and the error if any
-	connectResult = Signal(bool)
+	connectResult = pyqtSignal(bool)
+	returnAlbums = pyqtSignal(object, str)
+	returnAlbumSongs = pyqtSignal(object)
 
-	@Slot(str, str, str)
+	@pyqtSlot(str, str, str, result=bool)
 	def connectToServer(self, domain, username, password):
-		print(domain)
-		print('connecting')
+		print('connecting to {}'.format(domain))
 		domain = domain.strip()
-		if not domain[0:8] == "https://" or not domain[0:7] == "http://":
+		if not domain[0:8] == "https://" and not domain[0:7] == "http://":
 			domain = "https://" + domain
 		self.connection = Connection(domain, username, password, port=443, appName="airsonic-desktop",
 									 apiVersion="1.15.0")
 		ping = self.connection.ping()
+		print(ping)
 		self.connectResult.emit(ping)
 
-	@Slot()
-	def loadRandomAlbums(self):
-		print('getting random albums')
-		albums = self.connection.getAlbumList2('random', 50, 0)
+	@pyqtSlot(str)
+	def getAlbumsOfType(self, type):
+		print('getting {} albums'.format(type))
+		if type == "random":
+			albums = self.connection.getAlbumList2('random', 50, 0)
+		self.returnAlbums.emit(albums, type)
+
+	@pyqtSlot(int)
+	def getAlbumSongs(self, id):
+		print('getting songs for album {}'.format(id))
+		songs = self.connection.getAlbum(id)
+		self.returnAlbumSongs.emit(songs)
