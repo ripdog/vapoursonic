ebc042c61c02fdb34d4f7d44027ecfdcaa6959cd
ripdog <ripdog@users.noreply.github.com>
2019-12-25 20:25:14 +1300

    More work on build.yml.

--- .github/nsis.ps1
+++ .github/nsis.ps1
@@ -3,5 +3,5 @@ $nsisDir = (get-item nsis).FullName
 Invoke-WebRequest -Uri 'https://phoenixnap.dl.sourceforge.net/project/nsis/NSIS%203/3.05/nsis-3.05-setup.exe' -OutFile 'nsis-3.05-setup.exe'
 ls
 .\nsis-3.05-setup.exe' /S "/D=$nsisDir"
-$env:PATH += ';$nsisDir'
+$env:PATH += ";$nsisDir"
 .(get-item $env:LOCALAPPDATA\pypoetry\Cache\virtualenvs\vapoursonic-*-py3.6\Scripts\python.exe).FullName -m fbs installer
